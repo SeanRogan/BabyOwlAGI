72e8ae1507b53401e9c9408e9322492134201e06
Sean Rogan <74671024+SeanRogan@users.noreply.github.com>
2023-06-09 19:33:22 -0400

    requirements file and modifications to main

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ import Agents.worker_agent as worker_agent
 if __name__ == '__main__':
 
     print("\033[96m\033[1m"+"\n*****HOO HOO... How may i help you?*****\n"+"\033[0m\033[0m")
-    #OBJECTIVE = input()
-    OBJECTIVE = "Look up the cost of a new toyota camry in the boston massachusetts area."
-    # task_number = 1
+    OBJECTIVE = input()
     task_list = worker_agent.create_task_list(OBJECTIVE)
     worker_agent.print_task_list()
     while len(task_list) > 0:
@@ -14,3 +12,4 @@ if __name__ == '__main__':
                 worker_agent.execute_task(task, task_list, OBJECTIVE)
                 worker_agent.print_task_list()
                 break
+
